4cc17d6124c162fa29f9a9363f6d1c516349a2e5
Mohammed Sersawy <mohammed@sersawy.com>
2025-09-22 15:23:38 +0300

    fix router in not found page and login page

--- src/app/pages/not-found/not-found.ts
+++ src/app/pages/not-found/not-found.ts
@@ -1,9 +1,10 @@
 import { Component, inject } from '@angular/core';
 import { Location } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-not-found',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './not-found.html',
   styleUrl: './not-found.css',
 })
